2d9b5791335216562025d2c9155d1dc205787899
zach <zkunitsa@yahoo.com>
2018-10-10 18:35:39 +0000

    added files

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
   get '/' do
-    '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Hello World</title></head><body><h1>Hello World</h1></body></html>'
+    '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Hello World</title></head><body><h1>Goodby World</h1></body></html>'
   end
 end
